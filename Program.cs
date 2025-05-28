f9ebe1738563d432201abe2efa870cad701a4891
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-05-28 17:38:26 -0400

    Writing and Running Code in C# Assignment

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accouting Systems \nRemember, we're \"accounting\" for you!");
+            Console.Read();
+        }
+    }
+}
