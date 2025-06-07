03f031ee646526b285e6bc62cb176367e97b4a56
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-06-07 18:27:13 -0400

    Boolean Logic Assignment

--- Program.cs
+++ Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 
-namespace myConsoleProject.cs
+namespace BooleanLogicAssignment
 {
-    internal class Program
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Acme Accouting Systems \nRemember, we're \"accounting\" for you!");
-            Console.Read();
+            // ask the user for their age
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            //ask the user if they have ever had a DUI
+            Console.WriteLine("Have you ever had a DUI?");
+            string hasDUI = Console.ReadLine();
+            //ask the user how many speeding tickets they have
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speedingTickets = Console.ReadLine();
+            // check if the user is qualified for insurance
+            if (Convert.ToInt32(age) >= 15 && hasDUI == "false" && Convert.ToInt32(speedingTickets) <= 3)
+            {
+                Console.WriteLine("Qualified?");
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("Qualified?");
+                Console.WriteLine("false");
+            }
         }
     }
 }
