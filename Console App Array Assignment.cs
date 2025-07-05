62c6e7d223e32a4aefb4e7c5ea7eb81f28c1da55
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-05 16:34:54 -0400

    Console App Array Assignment

---
+++ Console App Array Assignment.cs
@@ -0,0 +1,14 @@
+using System;
+
+class ConsoleappArrayAssignment {
+    // This is the main entry point for the application.
+    
+    static void Main () {
+        string[] names = { Jon, Doe, Jane, Smith, Alice };
+        // Initializing an array of names
+        // Ask the user to select an index of the Array
+        Console.WriteLine("Please select an index of the Array (0-4):");
+        Console.ReadLine();
+        // Read the user input
+    }
+}
