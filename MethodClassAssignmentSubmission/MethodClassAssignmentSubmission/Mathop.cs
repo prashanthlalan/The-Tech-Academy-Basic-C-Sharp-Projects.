4f631e7e877cc4212fbbca279b6f5646840176b6
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-15 18:39:52 -0400

    Method Class Assignment Submission

---
+++ MethodClassAssignmentSubmission/MethodClassAssignmentSubmission/Mathop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignmentSubmission
+{
+    class Mathop
+    {
+        //Create a class. In that class, create a void method that takes two integers as parameters.
+        public void Add(int a, int b)
+        {
+            //Have the method do a math operation on the first integer
+            //and display the second integer to the screen.
+            int result = a + b;
+            Console.WriteLine("The sum of {0} and {1} is: {2}", a, b, result);
+        }
+    }
+}
---
+++ MethodClassAssignmentSubmission/MethodClassAssignmentSubmission/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignmentSubmission
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the Main() method of the console app, instantiate the class.
+            Mathop mathOperation = new Mathop();
+            //Call the method in the class, passing in two numbers.
+            mathOperation.Add(5, 10);
+            //Call the method in the class, specifying the parameters by name.
+            mathOperation.Add(a: 20, b: 30);
+        }
+    }
+}
