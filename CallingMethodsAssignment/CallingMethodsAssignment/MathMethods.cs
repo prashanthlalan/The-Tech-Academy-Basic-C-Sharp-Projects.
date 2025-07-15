10fe9fdfb07b9920ecdaa906db24acdf05511a56
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-15 19:52:42 -0400

    Calling Methods Assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/MathMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    public class MathMethods
+    {
+        //In that class, create three methods, each of which will take one integer parameter in and return an integer.
+        //The methods should do some math operation on the received parameter.
+        public int convertm(int km)
+        {
+            int metres = km * 1000;
+            return metres;
+        }
+        public int convertcm(int km)
+        {
+            int centimetres = km * 100000;
+            return centimetres;
+        }
+        public int convertmm(int km)
+        {
+            int millimetres = km * 1000000;
+            return millimetres;
+        }
+
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.WriteLine("Please enter the value in Killometre:");
+            //Convert the input to meters using parameters and return the result.
+            int km = Convert.ToInt32(Console.ReadLine());
+            MathMethods mathMethods = new MathMethods();
+            int metres = mathMethods.convertm(km);
+            Console.WriteLine("The value in Metres is: " + metres);
+
+            //Convert the input to centimeters using parameters and return the result.
+            int centimetres = mathMethods.convertcm(km);
+            Console.WriteLine("The value in Centimetres is: " + centimetres);
+
+            //Convert the input to millimeters using parameters and return the result.
+            int millimetres = mathMethods.convertmm(km);
+            Console.WriteLine("The value in Millimetres is: " + millimetres);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
