96afc152e5d91dc6eab3510e2dfe24509a4bb7a9
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-15 20:18:05 -0400

    Main Method Assignment

---
+++ MainMethodAssignment/MainMethodAssignment/MathMethods2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class MathMethods2
+    {
+        //First method that will take in an integer
+        public int MathOp(int num)
+        {
+            return num + 10;
+        }
+        //Second method that will take in a decimal
+        public decimal MathOp(decimal num)
+        {
+            //Convert the decimal to an integer, multiply by 100, and return as a decimal
+            int newnum =Convert.ToInt32(num);
+            return newnum * 100;
+        }
+        //Third method that will take in a string
+        public int MathOp(string num)
+        {
+            //Convert the string to an integer, multiply by 200 to perform the operation, and return as an integer
+            int newnum = Convert.ToInt32(num);
+            return (newnum * 200);
+        }
+
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initiate MathMethods2 class
+            MathMethods2 mathMethods = new MathMethods2();
+            // Call the first method with an integer parameter
+            Console.WriteLine(mathMethods.MathOp(5));
+            // Call the second method with a decimal parameter
+            Console.WriteLine(mathMethods.MathOp(4m));
+            // Call the third method with a string
+            Console.WriteLine(mathMethods.MathOp("4"));
+        }
+    }
+}
