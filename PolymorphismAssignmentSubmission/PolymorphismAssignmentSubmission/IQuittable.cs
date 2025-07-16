7c13ec20ca4ea41d4f7f015938c58be4ce5a7526
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-16 10:33:04 -0400

    Polymorphism Assignment Submission

---
+++ PolymorphismAssignmentSubmission/PolymorphismAssignmentSubmission/IQuittable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PolymorphismAssignmentSubmission
+{
+    // Creating IQuittable interface with Quit() method
+    public interface IQuittable
+    {
+        void Quit();
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+
+    // Employee class inheriting from Person and implementing IQuittable
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        // Implementing Quit() method from IQuittable interface
+        public void Quit()
+        {
+            Console.WriteLine(FirstName + " " + LastName + " has quit the company.");
+        }
+    }
+}
---
+++ PolymorphismAssignmentSubmission/PolymorphismAssignmentSubmission/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PolymorphismAssignmentSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
+            IQuittable employee = new Employee
+            {
+                FirstName = "Prashanth",
+                LastName = "Lalan",
+            };
+            employee.Quit();
+        }
+    }
+}
