2ab4d424336af9bef1b222d24c494c6e8a492731
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-16 11:55:26 -0400

    Operators Assignment Submission

---
+++ Operators Assignment Submission/Operators Assignment Submission/Compare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OperatorsAssignmentSubmission
+{
+    //Create an Employee class with Id, FirstName and LastName properties. 
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // In the Employee class, overload the “==” operator
+        // so it checks if two Employee objects are equal by comparing their Id property.
+        // Remember that comparison operators must be overloaded in pairs.
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            // Check if both are null
+            if (ReferenceEquals(emp1, emp2)) return true;
+            // Check if one is null
+            if (emp1 is null || emp2 is null) return false;
+            // Compare Ids
+            return emp1.Id == emp2.Id;
+        }
+        // Overload the “!=” operator to check if two Employee objects are not equal.
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            // Use the == operator to determine inequality
+            return !(emp1 == emp2);
+        }
+    }
+}
+        
---
+++ Operators Assignment Submission/Operators Assignment Submission/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OperatorsAssignmentSubmission
+{
+    class Program
+    {
+        //In the "Program.cs" file, instantiate two objects of the Employee class and assign values to their properties.
+        //Then compare the two Employee objects using the newly overloaded operators and display the results.
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee { Id = 1, FirstName = "Prashanth", LastName = "Lalan" };
+            Employee emp2 = new Employee { Id = 2, FirstName = "Robert", LastName = "De Niro" };
+            // Compare emp1 and emp2 when they are equal
+            Console.WriteLine($"Are emp1 and emp2 equal? {emp1 == emp2}");
+            // compare emp1 and emp2 when they are not equal
+            Console.WriteLine($"Are emp1 and emp2 not equal? {emp1 != emp2}");
+        }
+    }
+}
