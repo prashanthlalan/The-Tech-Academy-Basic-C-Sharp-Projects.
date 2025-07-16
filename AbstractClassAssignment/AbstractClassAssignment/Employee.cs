e9fee05671281983501965d19df371fd25060874
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-16 10:07:15 -0400

    Abstract Class Assignment

---
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    //Create another class called Employee and have it inherit from the Person class.
+    public class Employee : Person
+    {
+        //Give the Employee class a property called Id and override the SayName() method.
+        public int Id { get; set; }
+        //Implement the SayName() method inside of the Employee class.
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inside the Main() method, instantiate an Employee object with firstName
+            //“Sample” and lastName “Student”.
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+            };
+            //Call the SayName() method on the object.
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
