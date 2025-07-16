eb631df245e925ad2699712b61bbd5882c6a0514
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-16 14:35:48 -0400

    Constructor Assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Chain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConstructorAssignment.Chain
+{
+    public class Chain
+    {
+        //Constructor if one parameter, a float, is provided.
+        public Chain(float num1) : this(num1, "Prashanth")
+        {
+            Console.WriteLine(num1 + " and your default name is " + Name);
+        }
+        //Constructor if one parameter, a string, is provided.
+        public Chain(string name1) : this(39, name1)
+        {
+            Console.WriteLine(name1 + " and your default age is " + Num);
+        }
+        // Constructor if both parameters, a float and a string, are provided.
+        public Chain(float num, string name)
+        {
+            Num = num;
+            Name = name;
+        }
+        string Name { get; set; }
+        float Num { get; set; }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConstructorAssignment.Chain
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            // Create a const Variable
+            const string const1 = "Your Name is";
+            const string const2 = "Your age number is ";
+            Console.WriteLine("Enter your name or age: ");
+            // Create a variable using the var keyword
+            var answer = Console.ReadLine();
+            if (float.TryParse(answer, out float x))
+            {
+                Console.WriteLine(const2);
+                Chain chain1 = new Chain(x);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(const1);
+                Chain chain2 = new Chain(answer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
