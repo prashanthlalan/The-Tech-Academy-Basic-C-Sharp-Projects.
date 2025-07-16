2d0ff344fd7c64c052ba9e5dd3da1ec0383739bc
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-15 23:00:24 -0400

    Class Method Assignment

---
+++ ClassMethodAssignment/ClassMethodAssignment/MathMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    // Declaring a static class
+    public static class StaticMathOperations
+    {
+        public static void HalfValue(int input, out int result)
+        {
+            // Method with output parameter
+            result = input / 2;
+        }
+    }
+
+    // Creating a class with required methods
+    public class MathOperations
+    {
+        // Void method that divides input by 2 and outputs result
+        public void DivideByTwo(int number)
+        {
+            Console.WriteLine("Division result: " + (number / 2));
+        }
+
+        // First version of overloaded method
+        public void DisplayResult(int num)
+        {
+            Console.WriteLine("Your number is: " + num);
+        }
+
+        // Overloaded method with output parameter
+        public void DisplayResult(int num, out int divided)
+        {
+            divided = num / 2;
+        }
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class
+            MathOperations operations = new MathOperations();
+
+            // Handling user input
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Displaying division results
+            operations.DivideByTwo(number);
+
+            // Calling first overload method
+            operations.DisplayResult(number);
+
+            // Implementing method with output parameters
+            operations.DisplayResult(number, out int result);
+            Console.WriteLine("Output parameter result: " + result);
+
+            // Using static class
+            StaticMathOperations.HalfValue(number, out int staticResult);
+            Console.WriteLine("Static class result: " + staticResult);
+        }
+    }
+}
