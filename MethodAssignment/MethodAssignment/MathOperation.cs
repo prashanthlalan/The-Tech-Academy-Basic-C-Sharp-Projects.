74a411de54ef8529557b0be232bb9c2c17d30d05
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-15 20:56:36 -0400

    Method Assignment

---
+++ MethodAssignment/MethodAssignment/MathOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class MathOperation
+    {
+        //Create a method that takes two integers as parameters.
+        //Make one of them optional.
+        public int MathOp(int a, int b = 10)
+        {
+            //Have the method do a math operation and return an integer result.
+            return a * b;
+        }
+
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user to input two numbers, one at a time.
+            //Let them know they need not enter anything for the second number.
+            Console.WriteLine("Please enter the first number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number (optional, press Enter to skip):");
+            try
+            {
+                //Call the method in the class, if both parameters are given.
+                int secondnumber = Convert.ToInt32(Console.ReadLine());
+                int result = new MathOperation().MathOp(firstNumber, secondnumber);
+                Console.WriteLine(firstNumber + " x " + secondnumber + " = " + result);
+
+            }
+            catch
+            {
+                //Call the method in the class, if only one parameter is given.
+                int result = new MathOperation().MathOp(firstNumber);
+                Console.WriteLine(firstNumber + " x 10 = " + result);
+            }
+            Console.ReadLine();     
+        }
+    }
+}
