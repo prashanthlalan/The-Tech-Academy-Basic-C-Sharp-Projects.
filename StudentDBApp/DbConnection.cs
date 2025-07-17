666231fa00a3be1c8f8bb8398ff9316375109040
prashanthlalan <41975049+prashanthlalan@users.noreply.github.com>
2025-07-17 12:05:41 -0400

    Part 3

---
+++ StudentDBApp/DbConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentDBApp
+{
+    public class DbConnection : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=PRASHANTH\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;
+          TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ StudentDBApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using StudentDBApp;
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new DbConnection())
+        {
+            // Create database if not exists
+            context.Database.EnsureCreated();
+
+            // Add new student
+            var student = new Student
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            Console.WriteLine("Student added successfully!");
+            Console.WriteLine($"Student ID: {student.StudentId}");
+            Console.WriteLine($"Enrollment Date: {student.EnrollmentDate}");
+        }
+    }
+}
